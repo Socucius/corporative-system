bf841074bee7a1057f707f32f6acc557ac743af2
Alexander Shagov <x48j35@gmail.com>
2017-01-17 12:30:07 +0300

    Update customers table with required devise fields

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :confirmable, :validatable
+
   validates :first_name, :last_name, presence: true
 end
