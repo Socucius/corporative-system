a286313f4022815c90e5c59cb1202d924e4890a9
Alexander Shagov <x48j35@gmail.com>
2017-01-17 12:34:40 +0300

    Add devise routes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :confirmable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
 end
