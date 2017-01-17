083caae4e0fa458439410d3d5f8d96bb3271bece
Alexander Shagov <x48j35@gmail.com>
2017-01-17 16:43:48 +0300

    Add roles to employees model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,7 @@ class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :trackable, :rememberable, :validatable
+  enumerize :role, in: [:user, :admin], default: :user
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :role, presence: true
 end
