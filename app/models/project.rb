f9244a8224ef345a8b0e03e9e9839d6bc0325322
Alexander Shagov <x48j35@gmail.com>
2017-01-30 11:12:56 +0300

    Add status attribute to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
+  extend Enumerize
+  enumerize :status, in: [:new, :approved, :declined], default: :new, scope: true
+
   belongs_to :customer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117135927) do
+ActiveRecord::Schema.define(version: 20170130081222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20170117135927) do
     t.integer  "customer_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status"
     t.index ["customer_id"], name: "index_projects_on_customer_id", using: :btree
   end
 
